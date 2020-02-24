d3c2612b3c1d897dd08318bfcff0d1a235455221
Michael Schäfer <schaefer@digitalklang.de>
2020-02-24 23:33:37 +0100

    test 4

--- lsp/src/saros/lsp/extensions/path/PathFactory.java
+++ lsp/src/saros/lsp/extensions/path/PathFactory.java
@@ -4,26 +4,35 @@ import org.apache.log4j.Logger;
 
 import saros.filesystem.IPath;
 import saros.filesystem.IPathFactory;
+import saros.server.filesystem.ServerPathImpl;
 
-public class PathFactory implements IPathFactory {
+public class PathFactory implements IPathFactory {//TODO: use factory where static was used!
 
     private static final Logger LOG = Logger.getLogger(PathFactory.class);
 
     @Override
-    public String fromPath(IPath path) {
-        // TODO Auto-generated method stub
+  public String fromPath(IPath path) {
+    LOG.info(String.format("fromPath(%s)", path.toString()));
 
-        LOG.info(String.format("fromPath(%s)", path.toString()));
+    if (path == null) throw new NullPointerException("path is null");
 
-        return null;
-    }
+    return checkRelative(path).toString();
+  }
 
-    @Override
-    public IPath fromString(String name) {
-        // TODO Auto-generated method stub
-        LOG.info(String.format("fromString(%s)", name));
+  @Override
+  public IPath fromString(String name) {
+    LOG.info(String.format("fromString(%s)", name));
 
-        return null;
-    }
+    if (name == null) throw new NullPointerException("name is null");
+
+    return checkRelative(ServerPathImpl.fromString(name));
+  }
+
+  private IPath checkRelative(IPath path) {
+
+    if (path.isAbsolute()) throw new IllegalArgumentException("path is absolute: " + path);
+
+    return path;
+  }
 
 }
